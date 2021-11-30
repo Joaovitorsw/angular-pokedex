92658618fa97ae662c7df43ba37ed0fa152b79c0
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-11-30 15:29:30 -0300

    ✨ feat: create homepage component

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'px-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss']
+})
+export class HomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
