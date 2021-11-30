f096bc939b27a80ee407063902732e0e8d0d5364
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-11-30 15:30:39 -0300

    ✨ feat(app-component): import pages and change html to router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutPage } from './pages/about/about.page';
+import { HomePage } from './pages/home/home.page';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomePage, AboutPage],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomePage, data: { animation: 'HomePage' } },
+      { path: 'about', component: AboutPage, data: { animation: 'AboutPage' } },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
