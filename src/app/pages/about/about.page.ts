8dc5fa8b0f4432640a9fe356605b1ebf974e0dc3
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-11-30 15:29:55 -0300

    ✨ feat: create aboutpage component

---
+++ src/app/pages/about/about.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'px-about',
+  templateUrl: './about.page.html',
+  styleUrls: ['./about.page.scss']
+})
+export class AboutPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
