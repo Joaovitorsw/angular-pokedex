bb915fbfb35a281cc99450ee8cea13f37d28fc58
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-01 02:59:33 -0300

    ✨ feat(home-page): initial state

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import PokeAPI from 'pokedex-promise-v2';
+import { Observable } from 'rxjs';
+import { PokeAPIService } from 'src/app/services/poke-api/poke-api.service';
 
 @Component({
   selector: 'px-home',
   templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss']
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  items: Observable<PokeAPI.Pokemon[]>;
+  constructor(private pokeAPI: PokeAPIService) {
+    const indexArray = Array.from({ length: 15 }, (_, index) => ++index);
+    this.items = this.pokeAPI.getPokemonsByList(indexArray);
   }
 
+  ngOnInit(): void {}
 }
