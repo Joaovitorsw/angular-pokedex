d5e1498bbaf78f63e3be2cc12cd0f6ec6ff975f5
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-01 22:42:32 -0300

    ✨ feat: add angular-fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import PokeAPI from 'pokedex-promise-v2';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 import { AboutPage } from './pages/about/about.page';
 import { HomePage } from './pages/home/home.page';
-import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
+import { SpritePathPipe } from './pipes/sprite-path/sprite-path.pipe';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
-  declarations: [AppComponent, HomePage, AboutPage, PokemonCardComponent],
+  declarations: [
+    AppComponent,
+    HomePage,
+    AboutPage,
+    PokemonCardComponent,
+    SpritePathPipe,
+  ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     RouterModule.forRoot([
@@ -21,8 +36,12 @@ import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.com
         component: AboutPage,
       },
     ]),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
-  providers: [],
+  providers: [{ provide: PokeAPI, useValue: new PokeAPI() }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-pokedex-joaovitorsw',
+    appId: '1:921154066276:web:78c239b389275aebfa8c71',
+    storageBucket: 'angular-pokedex-joaovitorsw.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDvcbS_8vl_i0pfqP3mhW7hQLYodBl3rxg',
+    authDomain: 'angular-pokedex-joaovitorsw.firebaseapp.com',
+    messagingSenderId: '921154066276',
+    measurementId: 'G-LHMLWP7B4K',
+  },
   production: true
 };
