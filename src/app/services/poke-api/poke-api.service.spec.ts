9acc5c3b0248e244777ce1f14856701e95c7a3a6
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-01 22:44:54 -0300

    🧪 test: create poke api request file and files

--- src/app/services/poke-api/poke-api.service.spec.ts
+++ src/app/services/poke-api/poke-api.service.spec.ts
@@ -1,30 +1,44 @@
 import { TestBed } from '@angular/core/testing';
+import PokeAPI from 'pokedex-promise-v2';
 import { PokeAPIService } from './poke-api.service';
 
 describe('PokeAPIService', () => {
   let service: PokeAPIService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: PokeAPI, useValue: new PokeAPI() }],
+    });
     service = TestBed.inject(PokeAPIService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-  it('should return a pokemon', () => {
-    service.getPokemonByNameOrID(1).subscribe((pokemon) => {
-      expect(pokemon.name).toEqual('bulbasaur');
+
+  it('should return a pokemon', (done) => {
+    const pokemonID = 1;
+    const pokemonName = 'bulbasaur';
+    service.getPokemonByNameOrID(pokemonID).subscribe((pokemon) => {
+      expect(pokemon.name).toEqual(pokemonName);
+      done();
+    });
+    service.getPokemonByNameOrID(pokemonName).subscribe((pokemon) => {
+      expect(pokemon.name).toEqual(pokemonName);
+      done();
     });
   });
-  it('should return a list of pokemon', () => {
+
+  it('should return a list of pokemon', (done) => {
     const pokemonStringList = ['bulbasaur', 'ivysaur', 'venusaur'];
     const pokemonNumberList = [1, 2, 3];
     service.getPokemonsByList(pokemonStringList).subscribe((pokemons) => {
-      expect(pokemons.length).toBeGreaterThan(3);
+      expect(pokemons.length).toEqual(3);
+      done();
     });
     service.getPokemonsByList(pokemonNumberList).subscribe((pokemons) => {
-      expect(pokemons.length).toBeGreaterThan(3);
+      expect(pokemons.length).toEqual(3);
+      done();
     });
   });
 });
