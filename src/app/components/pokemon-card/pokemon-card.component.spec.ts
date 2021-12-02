8a13c9915097cfb14694dce8c524634a2b8dc9a0
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-01 22:43:41 -0300

    🧪 test: add component title test

--- src/app/components/pokemon-card/pokemon-card.component.spec.ts
+++ src/app/components/pokemon-card/pokemon-card.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import PokeAPI from 'pokedex-promise-v2';
+import { pokemonsMock } from 'src/app/mocks/pokemon-mocks';
+import { SpritePathPipe } from 'src/app/pipes/sprite-path/sprite-path.pipe';
 import { PokemonCardComponent } from './pokemon-card.component';
 
 describe('PokemonCardComponent', () => {
@@ -8,18 +10,28 @@ describe('PokemonCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PokemonCardComponent ]
-    })
-    .compileComponents();
+      declarations: [PokemonCardComponent, SpritePathPipe],
+      providers: [{ provide: PokeAPI, useValue: new PokeAPI() }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PokemonCardComponent);
     component = fixture.componentInstance;
+    component.pokemon = pokemonsMock[1];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it(`should have as pokemon`, () => {
+    expect(component.pokemon).toEqual(pokemonsMock[1]);
+  });
+
+  it('should render pokemon name', () => {
+    expect(fixture.nativeElement.querySelector('h1')?.textContent).toContain(
+      `${component.pokemon.name}`
+    );
   });
 });
