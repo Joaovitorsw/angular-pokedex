a9c16db294a6db4a50205e4ffc2a3f7031584d48
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-13 01:47:27 -0300

    ✨ feat(pokemon-card): type card

--- src/app/components/pokemon-card/pokemon-card.component.spec.ts
+++ src/app/components/pokemon-card/pokemon-card.component.spec.ts
@@ -2,17 +2,45 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import PokeAPI from 'pokedex-promise-v2';
 import { pokemonsMock } from 'src/app/mocks/pokemon-mocks';
 import { SpritePathPipe } from 'src/app/pipes/sprite-path/sprite-path.pipe';
+import { TypeIconPathPipe } from 'src/app/pipes/type-icon-path/type-icon-path.pipe';
+import { instance, mock } from 'ts-mockito';
 import { PokemonCardComponent } from './pokemon-card.component';
+
+const componentTemplateTest = ` 
+<h1>{{ pokemon?.name }}</h1>
+<p>{{ pokemon?.id }}</p>
+<div class="{{ slot.type.name }}  type " *ngFor="let slot of pokemon?.types">
+  <div class="icon {{ slot.type.name }}">
+    <img [src]="slot.type.name | typeIconPath" alt="" />
+  </div>
+  <p>{{ slot.type.name }}</p>
+</div>
+<p>{{ pokemon?.weight }}</p>
+<p>{{ pokemon?.height }}</p>
+`;
 
 describe('PokemonCardComponent', () => {
   let component: PokemonCardComponent;
   let fixture: ComponentFixture<PokemonCardComponent>;
-
+  let spritePathMock: SpritePathPipe;
+  let typePathMock: TypeIconPathPipe;
   beforeEach(async () => {
+    spritePathMock = mock(SpritePathPipe);
+    typePathMock = mock(TypeIconPathPipe);
+
+    const spritePathMockInstance = instance(spritePathMock);
+    const typePathMockInstance = instance(typePathMock);
+
     await TestBed.configureTestingModule({
-      declarations: [PokemonCardComponent, SpritePathPipe],
-      providers: [{ provide: PokeAPI, useValue: new PokeAPI() }],
-    }).compileComponents();
+      declarations: [PokemonCardComponent, SpritePathPipe, TypeIconPathPipe],
+      providers: [
+        { provide: PokeAPI, useValue: new PokeAPI() },
+        { provide: SpritePathPipe, useValue: spritePathMockInstance },
+        { provide: TypeIconPathPipe, useValue: typePathMockInstance },
+      ],
+    })
+      .overrideTemplate(PokemonCardComponent, componentTemplateTest)
+      .compileComponents();
   });
 
   beforeEach(() => {
