6971a344cb26ad3b8d867ab0cd311e264c92063e
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-13 01:45:46 -0300

    🧪 test(sprite-storage-service):  mocked testes situations

--- src/app/services/sprite-storage/sprite-storage.service.spec.ts
+++ src/app/services/sprite-storage/sprite-storage.service.spec.ts
@@ -1,17 +1,51 @@
 import { FirebaseApp } from '@angular/fire/app';
+import { of } from 'rxjs';
 import { instance, mock } from 'ts-mockito';
-import { SpriteStorageService } from './sprite-storage.service';
+import {
+  SpriteStorageErrorMessage,
+  SpriteStorageService,
+} from './sprite-storage.service';
 
 describe('SpriteStorageService', () => {
   let service: SpriteStorageService;
-  let firebaseAppMock: FirebaseApp;
+  let firebase: FirebaseApp;
   beforeEach(() => {
-    firebaseAppMock = mock(FirebaseApp);
-    const spriteStorageMockInstance = instance(firebaseAppMock);
-    service = new SpriteStorageService(spriteStorageMockInstance);
+    const firebaseApp = mock(FirebaseApp);
+    firebase = instance(firebaseApp);
+    service = new SpriteStorageService(firebaseApp);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be return the observable with gif path', (done) => {
+    const sample = 'bulbasaur';
+    const expected = 'bulbasaur.gif';
+    spyOn(service, 'getSpritePathByName').and.returnValue(of(expected));
+    service.getSpritePathByName(sample).subscribe((result) => {
+      expect(result).toBe(expected);
+      done();
+    });
+  });
+
+  it('should be return the observable with quota exceeded', (done) => {
+    const sample = 'bulbasaur';
+    const expected = SpriteStorageErrorMessage.QUOTA_EXCEEDED;
+    spyOn(service, 'getSpritePathByName').and.returnValue(of(expected));
+    service.getSpritePathByName(sample).subscribe((result) => {
+      expect(result).toBe(expected);
+      done();
+    });
+  });
+
+  it('should be return the observable with not found path ', (done) => {
+    const sample = 'bulbasaur';
+    const expected = SpriteStorageErrorMessage.NOT_FOUND;
+    spyOn(service, 'getSpritePathByName').and.returnValue(of(expected));
+    service.getSpritePathByName(sample).subscribe((result) => {
+      expect(result).toBe(expected);
+      done();
+    });
   });
 });
