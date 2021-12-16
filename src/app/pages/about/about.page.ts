1a1fd906e7e2353fdbaa178f8c8f42e7bb5ce24a
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-16 03:02:16 -0300

    ✨ feat: first template about

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Container, IOptions, RecursivePartial } from 'ng-particles';
+import { Pokemon } from 'pokedex-promise-v2';
+import { PokeAPIService } from 'src/app/services/poke-api/poke-api.service';
+import { particles, particlesAnimations } from 'src/assets/particles';
 
 @Component({
   selector: 'px-about',
   templateUrl: './about.page.html',
-  styleUrls: ['./about.page.scss']
+  styleUrls: ['./about.page.scss'],
 })
-export class AboutPage implements OnInit {
+export class AboutPage {
+  pokemon: Pokemon;
+  id = 'about-page';
+  type: string;
+  container: Container;
+  particlesOptions: RecursivePartial<IOptions>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private api: PokeAPIService) {
+    this.route.params.forEach(({ pokemonName }) => {
+      this.api.getPokemonByNameOrID(pokemonName).subscribe((pokemon) => {
+        this.pokemon = pokemon;
+        this.type = pokemon.types[0].type.name;
+        particlesAnimations[this.type]();
+        this.particlesOptions = particles;
+      });
+    });
   }
 
+  particlesLoaded(event: Container) {
+    event.actualOptions.load(particles);
+  }
 }
