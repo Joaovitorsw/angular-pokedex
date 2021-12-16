b32871ee484108417d5e8b6f56e1a5ead0620d79
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-16 03:01:56 -0300

    ✨ feat: particles presets

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
+import { IOptions, RecursivePartial } from 'ng-particles';
 import PokeAPI from 'pokedex-promise-v2';
 import { BehaviorSubject } from 'rxjs';
 import { PokeAPIService } from 'src/app/services/poke-api/poke-api.service';
-import { particles } from 'src/assets/particles';
-
-(particles as any).particles.shape.type = 'image';
-(particles as any).particles.shape.image.src =
-  'https://joaovitorsw-pokedex.netlify.app/search-bar-icon.a8e8adbe.svg';
+import { particles, particlesAnimations } from 'src/assets/particles';
 
 @Component({
   selector: 'px-home',
@@ -15,14 +12,19 @@ import { particles } from 'src/assets/particles';
 })
 export class HomePage {
   pokemons$$: BehaviorSubject<PokeAPI.Pokemon[]>;
-  id = 'ts-particles';
-  particlesOptions = particles;
+  particlesOptions: RecursivePartial<IOptions>;
+  start$;
+  id = 'home-page';
+  private pokeBallPath =
+    'https://joaovitorsw-pokedex.netlify.app/search-bar-icon.a8e8adbe.svg';
 
   constructor(private pokeAPI: PokeAPIService) {
     const pokemons$ = this.pokeAPI.getPokemonsByRange(1, 24);
-    pokemons$.subscribe((pokemons) => {
+    this.start$ = pokemons$.subscribe((pokemons) => {
       this.pokemons$$ = new BehaviorSubject(pokemons);
     });
+    particlesAnimations.homePage(this.pokeBallPath);
+    this.particlesOptions = particles;
   }
 
   endScroll() {
