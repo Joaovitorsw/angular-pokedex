f4ef66510093d8f75e89e3bb61ce25c0ab15e4b7
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-17 20:59:27 -0300

    📦 refactor(about-page): ts particle reload with hash change

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,21 +1,25 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Container, IOptions, RecursivePartial } from 'ng-particles';
+import { IOptions, RecursivePartial } from 'ng-particles';
 import { Pokemon } from 'pokedex-promise-v2';
 import { PokeAPIService } from 'src/app/services/poke-api/poke-api.service';
 import { particles, particlesAnimations } from 'src/assets/particles';
-
 @Component({
   selector: 'px-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage {
+  @HostListener('window:hashchange', ['$event'])
+  hashChangeHandler(): void {
+    this.hasChanged = false;
+  }
   @HostBinding('attr.type') type: string;
   pokemon: Pokemon;
   id = 'about-page';
-  container: Container;
   particlesOptions: RecursivePartial<IOptions>;
+  stats_conversion = 2;
+  hasChanged: boolean;
 
   constructor(private route: ActivatedRoute, private api: PokeAPIService) {
     this.route.params.forEach(({ pokemonName }) => {
@@ -24,11 +28,8 @@ export class AboutPage {
         this.type = pokemon.types[0].type.name;
         particlesAnimations[this.type]();
         this.particlesOptions = particles;
+        this.hasChanged = true;
       });
     });
-  }
-
-  particlesLoaded(event: Container) {
-    event.actualOptions.load(particles);
   }
 }
