5d7c32762e1764d1c53069979ead137f842e3235
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-22 07:13:19 -0300

    ✨ feat: enviroment dbConfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   firebase: {
     projectId: 'angular-pokedex-joaovitorsw',
@@ -13,7 +12,19 @@ export const environment = {
     messagingSenderId: '921154066276',
     measurementId: 'G-LHMLWP7B4K',
   },
-  production: false
+
+  dbConfig: {
+    name: 'db-pokedex-test',
+    version: 1,
+    objectStoresMeta: [
+      {
+        store: 'cached-pokemons',
+        storeConfig: { keyPath: 'test', autoIncrement: true },
+        storeSchema: [],
+      },
+    ],
+  },
+  production: true,
 };
 
 /*
