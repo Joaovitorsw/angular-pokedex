e86699fb2abd8d379d9c274748a83e354ddfb87d
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-22 07:27:21 -0300

    📦 refactor(home-page): remove logic the component to service

--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxIndexedDBModule } from 'ngx-indexed-db';
 import PokeAPI from 'pokedex-promise-v2';
+import { IndexedDbService } from 'src/app/services/indexed-db/indexed-db.service';
+import { PokeAPIService } from 'src/app/services/poke-api/poke-api.service';
+import { environment } from 'src/environments/environment';
 import { HomePage } from './home.page';
 
 describe('HomePage', () => {
@@ -9,7 +14,15 @@ describe('HomePage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomePage],
-      providers: [{ provide: PokeAPI, useValue: new PokeAPI() }],
+      imports: [
+        NgxIndexedDBModule.forRoot(environment.dbConfig),
+        RouterTestingModule,
+      ],
+      providers: [
+        PokeAPIService,
+        IndexedDbService,
+        { provide: PokeAPI, useValue: new PokeAPI() },
+      ],
     }).compileComponents();
   });
 
