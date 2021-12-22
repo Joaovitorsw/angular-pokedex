9d0cad7f2883d2a28c3e94651ee64da7eba2d3b1
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-22 07:27:21 -0300

    📦 refactor(home-page): remove logic the component to service

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,43 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Container, IOptions, RecursivePartial } from 'ng-particles';
-import PokeAPI from 'pokedex-promise-v2';
-import { BehaviorSubject } from 'rxjs';
 import { PokeAPIService } from 'src/app/services/poke-api/poke-api.service';
-import { particles, particlesAnimations } from 'src/assets/particles';
+import {
+  particles,
+  particlesAnimations,
+} from 'src/app/shared/ts-particles/particles.options';
 
 @Component({
   selector: 'px-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
-  pokemons$$: BehaviorSubject<PokeAPI.Pokemon[]>;
+export class HomePage implements OnInit {
   particlesOptions: RecursivePartial<IOptions>;
   container: Container;
   id = 'home-page';
 
-  constructor(private pokeAPI: PokeAPIService) {
-    const pokemons$ = this.pokeAPI.getPokemonsByRange(1, 24);
-    pokemons$.subscribe((pokemons) => {
-      this.pokemons$$ = new BehaviorSubject(pokemons);
-    });
+  constructor(public pokeAPI: PokeAPIService) {}
+
+  ngOnInit(): void {
     particlesAnimations.homePage();
     this.particlesOptions = particles;
+    this.pokeAPI.getPokemonsFirstRange();
   }
 
   endScroll() {
-    const pokemonsLength = this.pokemons$$.value.length + 1;
+    const pokemonsLength = this.pokeAPI.pokemons$$.value.length + 1;
     const nextLength = 24;
-    this.addPokemons(pokemonsLength, nextLength);
-  }
-
-  addPokemons(previous: number, next: number): void {
-    this.container.pause();
-    this.pokeAPI.getPokemonsByRange(previous, next).subscribe((pokemons) => {
-      const previousPokemons = this.pokemons$$.value;
-      this.pokemons$$.next([...previousPokemons, ...pokemons]);
-      this.container.play();
-    });
+    this.pokeAPI.nextPokemonsRange(pokemonsLength, nextLength);
   }
 
   particlesLoaded(container: Container) {
