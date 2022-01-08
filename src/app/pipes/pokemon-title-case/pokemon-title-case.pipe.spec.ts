9b88ea52e53409d6ec6f26ab5c5e1eb1048e0284
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-01-08 00:31:46 -0300

    ✨ feat: barrelsby

--- src/app/pipes/pokemon-title-case/pokemon-title-case.pipe.spec.ts
+++ src/app/pipes/pokemon-title-case/pokemon-title-case.pipe.spec.ts
@@ -1,21 +1,18 @@
 import { PokemonTitleCasePipe } from './pokemon-title-case.pipe';
 
-describe('PokemonTitleCasePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PokemonTitleCasePipe();
-    expect(pipe).toBeTruthy();
-  });
+const PIPE = new PokemonTitleCasePipe();
 
+describe('PokemonTitleCasePipe', () => {
   it('should return a title-case pokemon name', () => {
-    const pipe = new PokemonTitleCasePipe();
     const sample = 'bulbasaur';
     const expected = 'Bulbasaur';
-    expect(pipe.transform(sample)).toBe(expected);
+
+    expect(PIPE.transform(sample)).toBe(expected);
   });
   it('should return a title-case string in mega-forms', () => {
-    const pipe = new PokemonTitleCasePipe();
     const sample = 'mega-charizard-x';
     const expected = 'Mega Charizard X';
-    expect(pipe.transform(sample)).toBe(expected);
+
+    expect(PIPE.transform(sample)).toBe(expected);
   });
 });
