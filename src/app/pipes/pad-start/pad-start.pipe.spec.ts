5a315b5d3210bc97a328d504106db22579dbdd07
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-01-09 13:11:59 -0300

    🧪 test(pad-start pipe): remove case create an instance

--- src/app/pipes/pad-start/pad-start.pipe.spec.ts
+++ src/app/pipes/pad-start/pad-start.pipe.spec.ts
@@ -1,23 +1,20 @@
 import { PadStartPipe } from './pad-start.pipe';
 
-describe('PadStartPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PadStartPipe();
-    expect(pipe).toBeTruthy();
-  });
+const PIPE = new PadStartPipe();
 
+describe('PadStartPipe', () => {
   it('should return 00005', () => {
-    const pipe = new PadStartPipe();
-    expect(pipe.transform(5, 4, '0')).toBe('0005');
+    const expected = '0005';
+    expect(PIPE.transform(5, 4, '0')).toBe(expected);
   });
 
   it('should return  05', () => {
-    const pipe = new PadStartPipe();
-    expect(pipe.transform(5, 2, '0')).toBe('05');
+    const expected = '05';
+    expect(PIPE.transform(5, 2, '0')).toBe(expected);
   });
 
   it('should return  005', () => {
-    const pipe = new PadStartPipe();
-    expect(pipe.transform(5, 3, '0')).toBe('005');
+    const expected = '005';
+    expect(PIPE.transform(5, 3, '0')).toBe(expected);
   });
 });
