babfb2e8a9850285cc3a71bd4a8480f5b165a75a
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-01-09 14:01:18 -0300

    📦 refactor(pokemon-card component): remove oninit and constructor to get propertys

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  HostBinding,
-  Input,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { Component, HostBinding, Input, ViewChild } from '@angular/core';
 import PokeAPI from 'pokedex-promise-v2';
 
 @Component({
@@ -12,20 +6,15 @@ import PokeAPI from 'pokedex-promise-v2';
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.scss'],
 })
-export class PokemonCardComponent implements OnInit {
+export class PokemonCardComponent {
   @Input() pokemon: PokeAPI.Pokemon;
-  @HostBinding('attr.type') type: string;
   @ViewChild('pokemonImage') pokemonImage: HTMLImageElement;
-  stats_total: number;
+  @HostBinding('attr.type') get type(): string {
+    return this.pokemon.types[0].type.name;
+  }
+  get stats_total(): number {
+    return this.pokemon.stats.reduce((acc, cur) => acc + cur.base_stat, 0);
+  }
   stats_overall = 0.1;
   color: string;
-  constructor() {}
-
-  ngOnInit(): void {
-    this.type = this.pokemon.types[0].type.name;
-    this.stats_total = this.pokemon.stats.reduce(
-      (acc, cur) => acc + cur.base_stat,
-      0
-    );
-  }
 }
