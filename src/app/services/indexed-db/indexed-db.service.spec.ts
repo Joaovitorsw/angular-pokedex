fcdab6cd0e7db3ac48424b3ab15fd5bd90c5a7e8
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-01-09 13:08:36 -0300

    🧪 test(indexed-db service): remove case should be created and change const to upper snake case

--- src/app/services/indexed-db/indexed-db.service.spec.ts
+++ src/app/services/indexed-db/indexed-db.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { environment } from 'environments/environment';
 import { NgxIndexedDBModule } from 'ngx-indexed-db';
-import { environment } from 'src/environments/environment';
 import { eIndexDBKeys } from '../poke-api/poke-api.service';
 import { IndexedDbService } from './indexed-db.service';
 
 describe('IndexedDbService', () => {
   let service: IndexedDbService;
-  const key = '2';
+  const KEY = '2';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -16,12 +16,8 @@ describe('IndexedDbService', () => {
     service.clear(eIndexDBKeys.STORE).subscribe();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-
   it('should return observable from store of indexDb containing data', (done) => {
-    const data = { name: 'bulbasaur', test: key };
+    const data = { name: 'bulbasaur', test: KEY };
     const expected = 'bulbasaur';
     service.add(eIndexDBKeys.STORE, data).subscribe((key) => {
       expect(key.name).toEqual(expected);
@@ -30,7 +26,7 @@ describe('IndexedDbService', () => {
   });
 
   it('should return observable from store of indexDb updating data', (done) => {
-    const data = { name: 'ivysaur', test: key };
+    const data = { name: 'ivysaur', test: KEY };
     const expected = 'ivysaur';
     service.update(eIndexDBKeys.STORE, data).subscribe((res) => {
       expect(res[0].name).toEqual(expected);
@@ -39,7 +35,7 @@ describe('IndexedDbService', () => {
   });
 
   it('should return observable from store of indexDb deleting data', (done) => {
-    service.delete(eIndexDBKeys.STORE, key).subscribe((res) => {
+    service.delete(eIndexDBKeys.STORE, KEY).subscribe((res) => {
       expect(res.length).toEqual(0);
       done();
     });
@@ -53,7 +49,7 @@ describe('IndexedDbService', () => {
   });
 
   it('should return observable from store of indexDb getting all data', (done) => {
-    const data = { name: 'ivysaur', test: key };
+    const data = { name: 'ivysaur', test: KEY };
     service.add(eIndexDBKeys.STORE, data).subscribe((res) => {
       service.getAll(eIndexDBKeys.STORE).subscribe((res) => {
         expect(res.length).toEqual(1);
@@ -63,9 +59,9 @@ describe('IndexedDbService', () => {
   });
 
   it('should return observable from store of indexDb getting data by key', (done) => {
-    const data = { name: 'ivysaur', test: key };
+    const data = { name: 'ivysaur', test: KEY };
     service.add(eIndexDBKeys.STORE, data).subscribe((res) => {
-      service.getByKey(eIndexDBKeys.STORE, key).subscribe((res) => {
+      service.getByKey(eIndexDBKeys.STORE, KEY).subscribe((res) => {
         expect(res.name).toEqual('ivysaur');
         done();
       });
