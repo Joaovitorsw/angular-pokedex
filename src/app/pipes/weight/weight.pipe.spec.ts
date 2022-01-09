1616c83ee66334658a832caf98f2d5f1d94ca2d7
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-01-09 13:09:46 -0300

    🧪 test(weight pipe): remove case create an instance

--- src/app/pipes/weight/weight.pipe.spec.ts
+++ src/app/pipes/weight/weight.pipe.spec.ts
@@ -1,39 +1,31 @@
 import { WeightPipe } from './weight.pipe';
 
+const PIPE = new WeightPipe();
 describe('WeightPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new WeightPipe();
-    expect(pipe).toBeTruthy();
-  });
-
   it('should return 0.0 Kg if value is 0', () => {
-    const pipe = new WeightPipe();
     const value = 0;
-    const result = pipe.transform(value);
+    const result = PIPE.transform(value);
 
     expect(result).toBe('0.0 Kg');
   });
 
   it('should return 1.0 Kg if value is 10', () => {
-    const pipe = new WeightPipe();
     const value = 10;
-    const result = pipe.transform(value);
+    const result = PIPE.transform(value);
 
     expect(result).toBe('1.0Kg');
   });
 
   it('should return 10.0 Kg if value is 100', () => {
-    const pipe = new WeightPipe();
     const value = 100;
-    const result = pipe.transform(value);
+    const result = PIPE.transform(value);
 
     expect(result).toBe('10.0Kg');
   });
 
   it('should return 100.0 Kg if value is 1000', () => {
-    const pipe = new WeightPipe();
     const value = 1000;
-    const result = pipe.transform(value);
+    const result = PIPE.transform(value);
 
     expect(result).toBe('100.0Kg');
   });
