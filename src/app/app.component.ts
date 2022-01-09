f01f0208946d43717bb8538d06cda2460969fdf2
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-01-09 13:03:50 -0300

    📦 refactor(app component): change method to private and property names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,36 +4,36 @@ import { RouterOutlet } from '@angular/router';
 import { Container } from 'ng-particles';
 import { AboutPage } from './pages/about/about.page';
 import { HomePage } from './pages/home/home.page';
-import { slideInAnimation } from './shared/animations/slide.animation';
+import { SLIDE_IN_ANIMATION } from './shared/animations/slide.animation';
 
 @Component({
   selector: 'px-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [slideInAnimation],
+  animations: [SLIDE_IN_ANIMATION],
 })
 export class AppComponent {
   private container: Container;
-  states = true;
+  animationStatus = true;
 
   prepareRoute(outlet: RouterOutlet) {
     return outlet?.activatedRouteData?.['animation'];
   }
 
   onActivate(event: HomePage | AboutPage) {
-    const container = event.particlesEvent;
-    container.subscribe((container: Container) => {
+    const eventContainer = event.particlesEvent;
+    eventContainer.subscribe((container: Container) => {
       this.container = container;
-      this.animationStates(this.states);
+      this.particlesStatus(this.animationStatus);
     });
   }
 
   slideStatus(event: MatSlideToggleChange) {
-    this.states = event.checked;
-    this.animationStates(this.states);
+    this.animationStatus = event.checked;
+    this.particlesStatus(this.animationStatus);
   }
 
-  animationStates(state: boolean) {
+  private particlesStatus(state: boolean) {
     const method = state ? 'refresh' : 'stop';
     this.container[method]();
   }
