57ac4779a49081c6b9850bd25dad04f91278d32f
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-01-09 14:04:26 -0300

    🧪 test(slide-option component): use testing library and add omit case , output case and data test id in template

--- src/app/components/slide-option/slide-option.component.spec.ts
+++ src/app/components/slide-option/slide-option.component.spec.ts
@@ -1,31 +1,56 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  MatSlideToggleChange,
+  MatSlideToggleModule,
+} from '@angular/material/slide-toggle';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 import { SlideOptionComponent } from './slide-option.component';
 
+const DEFAULT_IMPORTS = [MatSlideToggleModule, MatIconModule];
+const DEFAULT_PROPERTIES = {
+  option: 'Animation',
+  icon: 'movie_creation',
+};
+
 describe('SlideOptionComponent', () => {
-  let component: SlideOptionComponent;
-  let fixture: ComponentFixture<SlideOptionComponent>;
+  it('emit MatSlideToggleChange change event', async () => {
+    await render(SlideOptionComponent, {
+      imports: DEFAULT_IMPORTS,
+      componentProperties: {
+        ...DEFAULT_PROPERTIES,
+        changeOption(event: MatSlideToggleChange) {
+          expect(event.checked).toBe(true);
+        },
+      },
+    });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [SlideOptionComponent],
-    }).compileComponents();
+    const $slideOption = screen.getByTestId('slide-option');
+    const $input = $slideOption.querySelector('input') as HTMLInputElement;
+
+    userEvent.click($input);
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SlideOptionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  it('emit @Output change event', async () => {
+    const changeSpy = jasmine.createSpy('change');
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    await render(SlideOptionComponent, {
+      imports: DEFAULT_IMPORTS,
+      componentProperties: {
+        ...DEFAULT_PROPERTIES,
+        change: {
+          emit: changeSpy,
+        } as any,
+        changeOption(event: MatSlideToggleChange) {
+          this.change!.emit(event);
+          expect(changeSpy).toHaveBeenCalled();
+        },
+      },
+    });
 
-  it('should emit event', () => {
-    const spy = spyOn(component.change, 'emit').and.callThrough();
-    const event = { checked: true } as MatSlideToggleChange;
-    component.changeOption(event);
-    expect(spy).toHaveBeenCalled();
+    const $slideOption = screen.getByTestId('slide-option');
+    const $input = $slideOption.querySelector('input') as HTMLInputElement;
+
+    userEvent.click($input);
   });
 });
