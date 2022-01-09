439b5fed9f454a4bb186dfa74c33a45992a93de7
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-01-09 13:56:54 -0300

    🧪 test(type-card component): update to testing library test

--- src/app/components/type-card/type-card.component.spec.ts
+++ src/app/components/type-card/type-card.component.spec.ts
@@ -1,40 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { pokemonsMock } from 'src/app/mocks/pokemon-mocks';
-import { TypeIconPathPipe } from 'src/app/pipes/type-icon-path/type-icon-path.pipe';
+import { render, screen } from '@testing-library/angular';
 import { TypeCardComponent } from './type-card.component';
 
+const DEFAULT_DECLARATIONS = [TypeCardComponent];
+const DEFAULT_PROPERTY_VALUES = {
+  type: 'fire',
+};
+
 describe('TypeCardComponent', () => {
-  let component: TypeCardComponent;
-  let fixture: ComponentFixture<TypeCardComponent>;
+  it('should render pokemon type', async () => {
+    await render(TypeCardComponent, {
+      declarations: DEFAULT_DECLARATIONS,
+      componentProperties: DEFAULT_PROPERTY_VALUES,
+    });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [TypeCardComponent, TypeIconPathPipe],
-    }).compileComponents();
+    const $typeCard = screen.getByTestId('type-card');
+    expect($typeCard.className).toEqual('fire');
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TypeCardComponent);
-    component = fixture.componentInstance;
-    component.type = pokemonsMock[0].types[0].type.name;
-    fixture.detectChanges();
-  });
+  it('should render type image', async () => {
+    await render(TypeCardComponent, {
+      declarations: DEFAULT_DECLARATIONS,
+      componentProperties: DEFAULT_PROPERTY_VALUES,
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    const $typeImage = screen
+      .getByTestId('type-card')
+      .querySelector('img') as HTMLImageElement;
 
-  it('should render pokemon type', () => {
-    const sample = fixture.nativeElement.querySelector('p').textContent;
-
-    const expected = component.type;
-    expect(sample).toContain(`${expected}`);
-  });
-
-  it('should render type image', () => {
-    const pipe = new TypeIconPathPipe();
-    const sample = fixture.nativeElement.querySelector('img').src;
-    const expected = pipe.transform(component.type);
-    expect(sample).toContain(`${expected}`);
+    const expected = 'assets/images/type-icons/fire.png';
+    expect($typeImage.getAttribute('src')).toEqual(expected);
   });
 });
