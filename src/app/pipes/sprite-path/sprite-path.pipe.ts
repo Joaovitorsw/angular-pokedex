ff375e5ead8667ba0fd11e76e327aa8416f850d7
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-01-20 07:10:05 -0300

    ✨ feat(sprite-path pipe): create private cache

--- src/app/pipes/sprite-path/sprite-path.pipe.ts
+++ src/app/pipes/sprite-path/sprite-path.pipe.ts
@@ -4,17 +4,21 @@ import {
   SpriteStorageService,
 } from '@pokedex/services';
 import PokeAPI from 'poke-api-models';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 @Pipe({
   name: 'spritePath',
 })
 export class SpritePathPipe implements PipeTransform {
   constructor(private readonly spriteStorage: SpriteStorageService) {}
+  private cache: string;
   transform({ name, sprites }: PokeAPI.Pokemon): Observable<string> {
+    if (this.cache) return of(this.cache);
+
     return this.spriteStorage.getSpritePathByName(name).pipe(
       map((spritePath) => {
         const NOT_FOUND = spritePath === SpriteStorageErrorMessage.NOT_FOUND;
+        this.cache = spritePath;
         if (!NOT_FOUND) return spritePath;
 
         const spriteVersionUrl =
