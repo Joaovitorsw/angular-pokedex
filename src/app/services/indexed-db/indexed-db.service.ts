36947f63afbd19e9a1b008a76957bc11f00cb149
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-01-20 05:11:21 -0300

    ✨ feat(indexed-db): use untildestroy in observables

--- src/app/services/indexed-db/indexed-db.service.ts
+++ src/app/services/indexed-db/indexed-db.service.ts
@@ -1,35 +1,41 @@
 import { Injectable } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { NgxIndexedDBService } from 'ngx-indexed-db';
-import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
+@UntilDestroy()
 export class IndexedDbService {
   constructor(private dbService: NgxIndexedDBService) {}
 
   getAll(storeName: string): Observable<any> {
-    return this.dbService.getAll(storeName).pipe(catchError(() => of(false)));
+    return this.dbService.getAll(storeName).pipe(untilDestroyed(this));
   }
 
   getByKey(storeName: string, key: any): Observable<any> {
-    return this.dbService.getByKey(storeName, key);
+    return this.dbService.getByKey(storeName, key).pipe(untilDestroyed(this));
   }
 
   add(storeName: string, value: any, key?: any): Observable<any> {
-    return this.dbService.add(storeName, value, key);
+    return this.dbService.add(storeName, value, key).pipe(untilDestroyed(this));
   }
 
   update(storeName: string, value: any, key?: any): Observable<any> {
-    return this.dbService.update(storeName, value, key);
+    return this.dbService
+      .update(storeName, value, key)
+      .pipe(untilDestroyed(this));
   }
 
   delete(storeName: string, key: string): Observable<Array<any>> {
-    return this.dbService.delete(storeName, key);
+    return this.dbService.delete(storeName, key).pipe(untilDestroyed(this));
   }
 
   clear(storeName: string): Observable<boolean> {
-    return this.dbService.clear(storeName);
+    return this.dbService.clear(storeName).pipe(untilDestroyed(this));
+  }
+  clearAll() {
+    return this.dbService.deleteDatabase().pipe(untilDestroyed(this));
   }
 }
