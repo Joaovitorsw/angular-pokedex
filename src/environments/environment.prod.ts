eebd6186b8b032d9dccd43361be432c14bd28d2c
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-01-20 05:07:11 -0300

    ✨ feat: remove enviroment informations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,25 +1,34 @@
 export const environment = {
   firebase: {
-    projectId: 'angular-pokedex-joaovitorsw',
-    appId: '1:921154066276:web:78c239b389275aebfa8c71',
-    storageBucket: 'angular-pokedex-joaovitorsw.appspot.com',
-    locationId: 'southamerica-east1',
-    apiKey: 'AIzaSyDvcbS_8vl_i0pfqP3mhW7hQLYodBl3rxg',
-    authDomain: 'angular-pokedex-joaovitorsw.firebaseapp.com',
-    messagingSenderId: '921154066276',
-    measurementId: 'G-LHMLWP7B4K',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    locationId: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+    measurementId: '',
   },
 
   dbConfig: {
-    name: 'pokedex',
+    name: '',
     version: 1,
     objectStoresMeta: [
       {
-        store: 'cached-pokemons',
-        storeConfig: { keyPath: 'id', autoIncrement: true },
+        store: '',
+        storeConfig: { keyPath: '', autoIncrement: true },
+        storeSchema: [],
+      },
+      {
+        store: '',
+        storeConfig: {
+          keyPath: '',
+          autoIncrement: false,
+          unique: true,
+        },
         storeSchema: [],
       },
     ],
   },
-  production: false,
+  production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,37 +1,34 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 export const environment = {
   firebase: {
-    projectId: 'angular-pokedex-joaovitorsw',
-    appId: '1:921154066276:web:78c239b389275aebfa8c71',
-    storageBucket: 'angular-pokedex-joaovitorsw.appspot.com',
-    locationId: 'southamerica-east1',
-    apiKey: 'AIzaSyDvcbS_8vl_i0pfqP3mhW7hQLYodBl3rxg',
-    authDomain: 'angular-pokedex-joaovitorsw.firebaseapp.com',
-    messagingSenderId: '921154066276',
-    measurementId: 'G-LHMLWP7B4K',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    locationId: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+    measurementId: '',
   },
 
   dbConfig: {
-    name: 'db-pokedex-test',
+    name: '',
     version: 1,
     objectStoresMeta: [
       {
-        store: 'cached-pokemons',
-        storeConfig: { keyPath: 'test', autoIncrement: true },
+        store: '',
+        storeConfig: { keyPath: '', autoIncrement: true },
+        storeSchema: [],
+      },
+      {
+        store: '',
+        storeConfig: {
+          keyPath: '',
+          autoIncrement: false,
+          unique: true,
+        },
         storeSchema: [],
       },
     ],
   },
-  production: true,
+  production: false,
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
