397c6e96786f3169a8e12e4b12f563cfbed662dd
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-01-25 13:47:11 -0300

    📦 refactor(sprite-storage service): return data url result

--- src/app/services/sprite-storage/sprite-storage.service.ts
+++ src/app/services/sprite-storage/sprite-storage.service.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, switchMap } from 'rxjs/operators';
+import { IndexedDbService } from '../indexed-db';
 
 export const enum SpriteStorageErrorCode {
   NOT_FOUND = '404',
@@ -21,7 +22,10 @@ interface SpriteData {
 })
 @UntilDestroy()
 export class SpriteStorageService {
-  constructor(private readonly http: HttpClient) {}
+  constructor(
+    private readonly http: HttpClient,
+    private indexDB: IndexedDbService
+  ) {}
   readonly BASE_URL =
     'https://raw.githubusercontent.com/Joaovitorsw/poke-gifs/main/normal/';
   readonly EXTENSION = '.gif';
@@ -30,7 +34,16 @@ export class SpriteStorageService {
     const url = `${this.BASE_URL}${name}${this.EXTENSION}`;
     return this.http.get(url, { responseType: 'blob' }).pipe(
       untilDestroyed(this),
-      map(() => url),
+      switchMap((blob) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(blob);
+        return new Observable<string>((observer) => {
+          fileReader.onload = () => {
+            observer.next(fileReader.result as string);
+            observer.complete();
+          };
+        });
+      }),
       catchError((error) => {
         if (error.status === 404)
           return of(SpriteStorageErrorMessage.NOT_FOUND);
