84e6021eae05c6d677a8b84633f048e4b67ba755
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-02-10 23:13:26 -0300

    ✨ feat(app-module): update new component import and mat addons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,14 +26,19 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DamageRelationsCardComponent } from './components/damage-relations-card/damage-relations-card.component';
+import { FlipCardComponent } from './components/flip-card/flip-card.component';
+import { PokemonBackCardComponent } from './components/pokemon-back-card/pokemon-back-card.component';
 import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 import { SlideOptionComponent } from './components/slide-option/slide-option.component';
+import { TableComponent } from './components/table/table.component';
 import { TypeCardComponent } from './components/type-card/type-card.component';
 import { ColorProgressBarDirective } from './directives/color-progress-bar/color-progress-bar.directive';
 import { PokemonSpriteDirective } from './directives/pokemon-sprite/pokemon-sprite.directive';
 import { ShowValidationErrorDirective } from './directives/show-validation-error';
 import { AboutPage } from './pages/about/about.page';
 import { HomePage } from './pages/home/home.page';
+import { FlavorTextPipe } from './pipes/flavor-text/flavor-text.pipe';
 import { HeightPipe } from './pipes/height/height.pipe';
 import { PadStartPipe } from './pipes/pad-start/pad-start.pipe';
 import { PokemonTitleCasePipe } from './pipes/pokemon-title-case/pokemon-title-case.pipe';
@@ -53,16 +61,24 @@ import { WeightPipe } from './pipes/weight/weight.pipe';
     ColorProgressBarDirective,
     SlideOptionComponent,
     PokemonSpriteDirective,
+    DamageRelationsCardComponent,
+    FlipCardComponent,
+    PokemonBackCardComponent,
+    FlavorTextPipe,
+    TableComponent,
   ],
   imports: [
     BrowserModule,
+    MatSortModule,
     MatInputModule,
     MatButtonModule,
     MatSelectModule,
+    MatTableModule,
     MatFormFieldModule,
     ReactiveFormsModule,
     MatSlideToggleModule,
     MatIconModule,
+    MatPaginatorModule,
     MatSidenavModule,
     NgxIndexedDBModule.forRoot(environment.dbConfig),
     MatProgressSpinnerModule,
