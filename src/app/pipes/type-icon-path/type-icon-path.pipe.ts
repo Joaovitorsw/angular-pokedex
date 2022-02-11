fb35add9b3e873a9a93be524dfca904545bd2104
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-02-10 23:10:54 -0300

    📦 refactor(type-icon-path pipe): accept undefined in parameter

--- src/app/pipes/type-icon-path/type-icon-path.pipe.ts
+++ src/app/pipes/type-icon-path/type-icon-path.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TypeIconPathPipe implements PipeTransform {
   readonly BASE_URL = 'assets/images/type-icons/';
   readonly EXTENSION = '.png';
-  transform(value: string): string {
+  transform(value: string | undefined): string {
     const imagePath = this.BASE_URL + value + this.EXTENSION;
     return imagePath;
   }
