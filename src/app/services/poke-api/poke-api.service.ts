d1d5c6323170dcf31d82d0051746f0b2e5990c0c
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-02-10 23:06:56 -0300

    ✨ feat(poke-api service): create ability and move request

--- src/app/services/poke-api/poke-api.service.ts
+++ src/app/services/poke-api/poke-api.service.ts
@@ -3,14 +3,16 @@ import { Injectable } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { POKEMONS } from '@pokedex/pages';
 import {
+  Ability,
   EvolutionChain,
+  Move,
   Pokemon,
   PokemonEvolutions,
   PokemonSpecies,
   Variety,
 } from 'poke-api-models';
-import { BehaviorSubject, defer, Observable, of } from 'rxjs';
-import { catchError, switchMap } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest, defer, Observable, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -20,11 +22,15 @@ export class PokeAPIService {
   constructor(private http: HttpClient) {}
   readonly BASE_URL = 'https://pokeapi.co/api/v2/';
   readonly POKEMON_SPECIES_EXTENSION = 'pokemon-species/';
+  readonly MOVE_EXTENSION = 'move/';
   readonly POKEMON_EXTENSION = 'pokemon/';
+  readonly ABILITY_EXTENSION = 'ability/';
   readonly EVOLUTION_CHAIN_EXTENSION = 'evolution-chain/';
   request$$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   pokemons$$ = new BehaviorSubject<Pokemon[]>([]);
   pokemons: Pokemon[] = POKEMONS;
+  pokemonForms: Pokemon[];
+  pokemonMoves: Move[];
 
   getPokemonByNameOrID(name: string | number): Observable<Pokemon> {
     return this.http
@@ -131,6 +137,12 @@ export class PokeAPIService {
           allSpecies.indexOf(element)
         );
         const variables = await this.getPokemonVariates(filteredSpecies);
+        const flavor_text_entries = species.flavor_text_entries.filter(
+          (entry) => entry.language.name === 'en'
+        )[0];
+        flavor_text_entries.flavor_text =
+          flavor_text_entries.flavor_text.replace('POKéMON', 'pokémon');
+        species.flavor_text_entries = [flavor_text_entries];
 
         const evolution: PokemonEvolutions = {
           species: species,
@@ -200,5 +212,64 @@ export class PokeAPIService {
     ).toPromise();
 
     return variables;
+  }
+
+  getPokemonAbility(id: string): Observable<Ability> {
+    return this.http
+      .get<Ability>(`${this.BASE_URL}${this.ABILITY_EXTENSION}${id}`)
+      .pipe(untilDestroyed(this));
+  }
+
+  getPokemonMove(id: string): Observable<Move> {
+    return this.http
+      .get<Move>(`${this.BASE_URL}${this.MOVE_EXTENSION}${id}`)
+      .pipe(untilDestroyed(this));
+  }
+
+  getPokemonAbilities(ids: string[]): Observable<Ability[]> {
+    const ids$ = ids.map((id) => this.getPokemonAbility(id));
+    const abilities$ = combineLatest(ids$).pipe(
+      map((abilities) => {
+        const formattedAbilities = abilities.map((ability) => {
+          const flavor_text_entries = ability.flavor_text_entries.filter(
+            (entry) => entry.language.name === 'en'
+          );
+
+          const effect_entries = ability.effect_entries.filter(
+            (entry) => entry.language.name === 'en'
+          );
+          ability.effect_entries = effect_entries;
+          ability.flavor_text_entries = [
+            flavor_text_entries[flavor_text_entries.length - 1],
+          ];
+          return ability;
+        });
+
+        return formattedAbilities;
+      }),
+      catchError((error) => of(error))
+    );
+    return abilities$;
+  }
+
+  getMoveList(ids: string[]): Observable<Move[]> {
+    const ids$ = ids.map((id) => this.getPokemonMove(id));
+    const moves$ = combineLatest(ids$).pipe(
+      map((moves) => {
+        const formattedMoves = moves.map((move) => {
+          const flavor_text_entries = move.flavor_text_entries.filter(
+            (entry) => entry.language.name === 'en'
+          );
+          move.flavor_text_entries = [
+            flavor_text_entries[flavor_text_entries.length - 1],
+          ];
+          this.pokemonMoves = moves;
+          return move;
+        });
+
+        return formattedMoves;
+      })
+    );
+    return moves$;
   }
 }
