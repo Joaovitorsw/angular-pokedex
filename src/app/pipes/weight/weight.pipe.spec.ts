357f5ede03af5a7fcc13aebc732c9bcc1f416ac7
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-02-12 06:01:23 -0300

    ✨ feat: weight pipe

---
+++ src/app/pipes/weight/weight.pipe.spec.ts
@@ -0,0 +1,32 @@
+import { WeightPipe } from './weight.pipe';
+
+const pipe = new WeightPipe();
+describe('WeightPipePipe', () => {
+  it('should return 0.0 Kg if value is 0', () => {
+    const value = 0;
+    const result = pipe.transform(value);
+
+    expect(result).toBe('0.0 Kg');
+  });
+
+  it('should return 1.0 Kg if value is 10', () => {
+    const value = 10;
+    const result = pipe.transform(value);
+
+    expect(result).toBe('1.0Kg');
+  });
+
+  it('should return 10.0 Kg if value is 100', () => {
+    const value = 100;
+    const result = pipe.transform(value);
+
+    expect(result).toBe('10.0Kg');
+  });
+
+  it('should return 100.0 Kg if value is 1000', () => {
+    const value = 1000;
+    const result = pipe.transform(value);
+
+    expect(result).toBe('100.0Kg');
+  });
+});
