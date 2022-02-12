23236afdebc3d78eee8b50bb055e071face6ae0e
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-02-12 06:05:07 -0300

    📦 refactor(short-pokemon-card): use weight,height and padstart pipes in template

--- src/app/domain/home/home.domain.module.ts
+++ src/app/domain/home/home.domain.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ShortPokemonCardComponent } from 'app/components/short-pokemon-card/short-pokemon-card.component';
 import { ColorProgressBarDirective } from 'app/directives/color-progress-bar';
+import { HeightPipe } from 'app/pipes/height';
+import { PadStartPipe } from 'app/pipes/pad-start';
+import { WeightPipe } from 'app/pipes/weight';
 import { HomeDomainRoutingModule } from './home.domain-routing.module';
 import { HomePage } from './pages';
 
@@ -11,6 +14,9 @@ import { HomePage } from './pages';
     HomePage,
     ShortPokemonCardComponent,
     ColorProgressBarDirective,
+    WeightPipe,
+    HeightPipe,
+    PadStartPipe,
   ],
   imports: [CommonModule, HomeDomainRoutingModule, MatProgressBarModule],
 })
