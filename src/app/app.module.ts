8c10e49e46b493f8a04cc56dff052c8d0c4618b2
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-02-17 13:03:55 -0300

    📦 refactor: remove unnecessary files and lines

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,9 +19,7 @@ import { AppComponent } from './app.component';
     }),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerWhenStable:30000',
     }),
   ],
 
