c718561c087997ed8050a13f99b138e544673175
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-02-17 13:04:33 -0300

    ✨ feat(short-pokemon-card): adding type container and weaness

--- src/app/components/short-pokemon-card/short-pokemon-card.component.ts
+++ src/app/components/short-pokemon-card/short-pokemon-card.component.ts
@@ -3,7 +3,10 @@ import {
   Component,
   HostBinding,
   Input,
+  OnChanges,
+  SimpleChanges,
 } from '@angular/core';
+import { DAMAGE_RELATIONS } from '@pokedex/database';
 import { ShortPokemon } from 'poke-api-models';
 
 @Component({
@@ -12,13 +15,44 @@ import { ShortPokemon } from 'poke-api-models';
   styleUrls: ['./short-pokemon-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShortPokemonCardComponent {
+export class ShortPokemonCardComponent implements OnChanges {
   stats_overall = 0.1;
   color: string;
+  damageFrom: string;
+  weakness: string;
+  quadDamage: boolean;
+
+  @Input('weakness') selectedWeakness?: string | null;
   @Input() pokemon: ShortPokemon;
   @HostBinding('attr.type') get type(): string {
     return this.pokemon.types[0].type.name;
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['selectedWeakness']) {
+      const pokemonWeakness = this.pokemon.types.map((type) => {
+        return DAMAGE_RELATIONS.filter(
+          (relation) => relation.name === type.type.name
+        );
+      });
+
+      const doubleDamage = pokemonWeakness
+        .flatMap((weakness) => weakness)
+        .flatMap((weakness) => weakness.damage_relations.double_damage_from);
+
+      const mostWeakness = doubleDamage.filter(
+        (elemento, index, array) => !(array.indexOf(elemento) === index)
+      );
+
+      this.quadDamage = mostWeakness.includes(this.selectedWeakness ?? '');
+      this.damageFrom = this.quadDamage ? '4X' : '2X';
+
+      this.weakness =
+        doubleDamage.find((weakness) => weakness === this.selectedWeakness) ??
+        mostWeakness.find((weakness) => weakness === this.selectedWeakness)!;
+    }
+  }
+
   get stats_total(): number {
     return this.pokemon.stats.reduce((acc, stat) => acc + stat.base_stat, 0);
   }
