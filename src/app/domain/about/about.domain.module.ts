f5a04872b84a3e3e21f9f3c9dbb04248a330b1fa
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-02-17 18:12:09 -0300

    ✨ feat(about page): ng particles for background

--- src/app/domain/about/about.domain.module.ts
+++ src/app/domain/about/about.domain.module.ts
@@ -6,8 +6,7 @@ import { PadStartWidgetModule } from 'app/pipes/pad-start/pad-start.module';
 import { PokemonTextTitleCaseWidgetModule } from 'app/pipes/pokemon-text-title-case/pokemon-text-title-case.module';
 import { WeightWidgetModule } from 'app/pipes/weight/weight.module';
 import { NgParticlesModule } from 'ng-particles';
-import { AboutDomainRoutingModule } from './about.domain-routing.module';
-import { AboutPage } from './pages/about/about.page';
+import { AboutDomainRoutingModule, AboutPage } from '.';
 
 @NgModule({
   declarations: [AboutPage],
--- src/app/domain/about/pages/about/about.page.ts
+++ src/app/domain/about/pages/about/about.page.ts
@@ -1,16 +1,52 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokeApiService } from '@pokedex/services';
+import { particles, particlesAnimations } from 'app/shared/ts-particles';
+import { IOptions, RecursivePartial } from 'ng-particles';
+import { Pokemon } from 'poke-api-models';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'px-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AboutPage implements OnInit {
+export class AboutPage implements OnInit, OnDestroy {
+  @HostBinding('attr.type') type: string;
+  pokemon$: Observable<Pokemon>;
+  hasChanged: boolean;
+  particlesOptions: RecursivePartial<IOptions>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, public pokeAPI: PokeApiService) {}
 
   ngOnInit(): void {
+    this.route.params.forEach(({ pokemonName }) => {
+      this.hasChanged = false;
+
+      this.pokemon$ = this.pokeAPI.getPokemonByNameOrID(pokemonName).pipe(
+        tap((pokemon: Pokemon) => {
+          this.setBodyClass(pokemon);
+          this.getAnimation();
+        })
+      );
+    });
   }
 
+  ngOnDestroy(): void {
+    const $body = document.body;
+    $body.classList.remove(this.type);
+  }
+
+  setBodyClass(pokemon: Pokemon) {
+    const $body = document.body;
+    $body.classList.remove(this.type);
+    this.type = pokemon.types[0].type.name;
+    $body.classList.add(this.type);
+  }
+
+  getAnimation() {
+    particlesAnimations[this.type]();
+    this.particlesOptions = particles;
+    this.hasChanged = true;
+  }
 }
