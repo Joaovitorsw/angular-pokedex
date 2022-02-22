868fb311d22fc81b0e28912fcc366f8fa5645c96
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-02-22 16:31:53 -0300

    ✨ feat(poke-api service): ability, evolution chain and move endpoints

--- src/app/services/poke-api/poke-api.service.spec.ts
+++ src/app/services/poke-api/poke-api.service.spec.ts
@@ -3,6 +3,13 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import {
+  BULBASAUR,
+  BULBASAUR_ABILITIES,
+  BULBASAUR_EVOLUTION_CHAIN,
+  BULBASAUR_MOVES,
+  BULBASAUR_SPECIES,
+} from '@pokedex/mocks';
 import { SHORT_POKEMONS } from 'app/database/short-pokemons';
 import { Pokemon } from 'poke-api-models';
 import { PokeApiService } from './poke-api.service';
@@ -46,6 +53,108 @@ describe('PokeAPIService', () => {
       );
       expect(req.request.method).toBe('GET');
       req.flush(pokemon);
+    });
+  });
+
+  it('should return a pokemon species by name or id', (done) => {
+    service.getSpeciesByNameOrID(BULBASAUR.name).subscribe((res) => {
+      expect(res).toEqual(BULBASAUR_SPECIES);
+      done();
+    });
+    const req = httpMock.expectOne(
+      `${service.BASE_URL}${service.POKEMON_SPECIES_EXTENSION}${BULBASAUR.name}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(BULBASAUR_SPECIES);
+  });
+
+  it('should return a list of pokemons species by list', (done) => {
+    const pokemons = [BULBASAUR];
+    const expectedSpecies = [BULBASAUR_SPECIES];
+    const pokemonsList = pokemons.map((pokemon) => pokemon.name);
+    service.getSpeciesByList(pokemonsList).subscribe((res) => {
+      expect(res).toEqual(expectedSpecies);
+      done();
+    });
+
+    pokemons.forEach((pokemon) => {
+      const req = httpMock.expectOne(
+        `${service.BASE_URL}${service.POKEMON_SPECIES_EXTENSION}${pokemon.name}`
+      );
+      expect(req.request.method).toBe('GET');
+      req.flush(BULBASAUR_SPECIES);
+    });
+  });
+
+  it('should return a evolution chain of pokemon by name or id', (done) => {
+    service.getEvolutionChainById(BULBASAUR.id).subscribe((res) => {
+      expect(res).toEqual(BULBASAUR_EVOLUTION_CHAIN);
+      done();
+    });
+    const req = httpMock.expectOne(
+      `${service.BASE_URL}${service.EVOLUTION_CHAIN_EXTENSION}${BULBASAUR.id}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(BULBASAUR_EVOLUTION_CHAIN);
+  });
+
+  it('should return a pokemon ability by name or id', (done) => {
+    service
+      .getAbilityByNameOrID(BULBASAUR.abilities[0].ability.name)
+      .subscribe((res) => {
+        expect(res).toEqual(BULBASAUR_ABILITIES[0]);
+        done();
+      });
+    const req = httpMock.expectOne(
+      `${service.BASE_URL}${service.ABILITY_EXTENSION}${BULBASAUR.abilities[0].ability.name}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(BULBASAUR_ABILITIES[0]);
+  });
+
+  it('should return a list of pokemons abilities by list', (done) => {
+    const abilities = BULBASAUR.abilities.map(
+      (ability) => ability.ability.name
+    );
+    service.getAbilitiesByList(abilities).subscribe((res) => {
+      expect(res).toEqual(BULBASAUR_ABILITIES);
+      done();
+    });
+
+    abilities.forEach((ability, index) => {
+      const req = httpMock.expectOne(
+        `${service.BASE_URL}${service.ABILITY_EXTENSION}${ability}`
+      );
+      expect(req.request.method).toBe('GET');
+      req.flush(BULBASAUR_ABILITIES[index]);
+    });
+  });
+
+  it('should return a pokemon move by name or id', (done) => {
+    service.getMoveByNameOrID(BULBASAUR.moves[0].move.name).subscribe((res) => {
+      expect(res).toEqual(BULBASAUR_MOVES[0]);
+      done();
+    });
+    const req = httpMock.expectOne(
+      `${service.BASE_URL}${service.MOVE_EXTENSION}${BULBASAUR.moves[0].move.name}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(BULBASAUR_MOVES[0]);
+  });
+
+  it('should return a list of pokemons moves by list', (done) => {
+    const moves = BULBASAUR.moves.map((move) => move.move.name);
+    service.getMovesByList(moves).subscribe((res) => {
+      expect(res).toEqual(BULBASAUR_MOVES);
+      done();
+    });
+
+    moves.forEach((move, index) => {
+      const req = httpMock.expectOne(
+        `${service.BASE_URL}${service.MOVE_EXTENSION}${move}`
+      );
+      expect(req.request.method).toBe('GET');
+      req.flush(BULBASAUR_MOVES[index]);
     });
   });
 });
--- src/app/services/poke-api/poke-api.service.ts
+++ src/app/services/poke-api/poke-api.service.ts
@@ -1,7 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { Pokemon, ShortPokemon } from 'poke-api-models';
+import {
+  Ability,
+  EvolutionChain,
+  Move,
+  Pokemon,
+  PokemonSpecies,
+  ShortPokemon,
+} from 'poke-api-models';
 import { combineLatest, Observable } from 'rxjs';
 
 @Injectable({
@@ -11,6 +18,11 @@ import { combineLatest, Observable } from 'rxjs';
 export class PokeApiService {
   readonly BASE_URL = 'https://pokeapi.co/api/v2/';
   readonly POKEMON_EXTENSION = 'pokemon/';
+  readonly POKEMON_SPECIES_EXTENSION = 'pokemon-species/';
+  readonly MOVE_EXTENSION = 'move/';
+  readonly ABILITY_EXTENSION = 'ability/';
+  readonly EVOLUTION_CHAIN_EXTENSION = 'evolution-chain/';
+
   simplifiedPokemons: ShortPokemon[];
   constructor(private http: HttpClient) {}
 
@@ -25,5 +37,52 @@ export class PokeApiService {
       this.getPokemonByNameOrID(nameOrID)
     );
     return combineLatest(pokemons$);
+  }
+
+  getSpeciesByNameOrID(nameOrID: string | number): Observable<PokemonSpecies> {
+    return this.http
+      .get<PokemonSpecies>(
+        `${this.BASE_URL}${this.POKEMON_SPECIES_EXTENSION}${nameOrID}`
+      )
+      .pipe(untilDestroyed(this));
+  }
+
+  getSpeciesByList(list: Array<number | string>): Observable<PokemonSpecies[]> {
+    const species$ = list.map((nameOrID) =>
+      this.getSpeciesByNameOrID(nameOrID)
+    );
+    return combineLatest(species$);
+  }
+
+  getEvolutionChainById(id: number): Observable<EvolutionChain> {
+    return this.http
+      .get<EvolutionChain>(
+        `${this.BASE_URL}${this.EVOLUTION_CHAIN_EXTENSION}${id}`
+      )
+      .pipe(untilDestroyed(this));
+  }
+
+  getAbilityByNameOrID(nameOrID: number | string): Observable<Ability> {
+    return this.http
+      .get<Ability>(`${this.BASE_URL}${this.ABILITY_EXTENSION}${nameOrID}`)
+      .pipe(untilDestroyed(this));
+  }
+
+  getAbilitiesByList(list: Array<number | string>): Observable<Ability[]> {
+    const abilities$ = list.map((nameOrID) =>
+      this.getAbilityByNameOrID(nameOrID)
+    );
+    return combineLatest(abilities$);
+  }
+
+  getMoveByNameOrID(nameOrID: string | number): Observable<Move> {
+    return this.http
+      .get<Move>(`${this.BASE_URL}${this.MOVE_EXTENSION}${nameOrID}`)
+      .pipe(untilDestroyed(this));
+  }
+
+  getMovesByList(list: Array<number | string>): Observable<Move[]> {
+    const moves$ = list.map((nameOrID) => this.getMoveByNameOrID(nameOrID));
+    return combineLatest(moves$);
   }
 }
